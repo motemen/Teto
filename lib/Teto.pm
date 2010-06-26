00438a85d531a161d4815ca14e843cfc5e1f046c
motemen <motemen@gmail.com>
2010-06-26 23:32:06 +0900

    initial commit

---
+++ lib/Teto.pm
@@ -0,0 +1,34 @@
+package Teto;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Teto -
+
+=head1 SYNOPSIS
+
+  use Teto;
+
+=head1 DESCRIPTION
+
+Teto is
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
