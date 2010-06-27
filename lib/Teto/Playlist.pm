0ae001c74384db9243fb27f694a70b24e0ab7bfc
motemen <motemen@gmail.com>
2010-06-28 03:16:11 +0900

    Playlist

---
+++ lib/Teto/Playlist.pm
@@ -0,0 +1,20 @@
+package Teto::Playlist;
+use Any::Moose;
+
+has 'entries', (
+    is  => 'rw',
+    isa => 'ArrayRef[Teto::Playlist::Entry]',
+    default => sub { [] },
+);
+
+__PACKAGE__->meta->make_immutable;
+
+use Teto::Playlist::Entry;
+
+sub add_entry {
+    my $self = shift;
+    my $entry = ref $_[0] eq 'HASH' ? $_[0] : { @_ };
+    push @{$self->entries}, Teto::Playlist::Entry->new($entry);
+}
+
+1;
---
+++ lib/Teto/Playlist/Entry.pm
@@ -0,0 +1,32 @@
+package Teto::Playlist::Entry;
+use Any::Moose;
+
+has 'title', (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+has 'url', (
+    is  => 'rw',
+    isa => 'Str', # XXX URI
+);
+
+has 'source_url', (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+has 'image_url', (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+__PACKAGE__->meta->make_immutable;
+
+use overload
+    '""' => 'stringify',
+    fallback => 1;
+
+sub stringify { "$_[0]->{title} <$_[0]->{url}> from $_[0]->{source_url}" }
+
+1;
