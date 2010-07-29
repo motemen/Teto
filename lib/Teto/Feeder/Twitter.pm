acb24a031b6c1a854cb0cedf3c90b99e2f09ef4c
motemen <motemen@gmail.com>
2010-07-29 19:43:36 +0900

    短縮 URL 対応

--- lib/Teto/Feeder/Twitter.pm
+++ lib/Teto/Feeder/Twitter.pm
@@ -18,6 +18,36 @@ has 'guard', (
     builder => '_build_guard',
 );
 
+our $SHORT_URL_SERVICE_RE = qr(
+    http://bit\.ly/\w+
+        |
+    http://j\.mp/\w+
+        |
+    http://nico\.ms/\w+
+        |
+    http://htn\.to/\w+
+)x;
+
+sub url_is_expandable {
+    my ($url) = @_;
+    return $url =~ $SHORT_URL_SERVICE_RE;
+}
+
+sub expand_short_url {
+    my ($url, $cb) = @_;
+
+    if (url_is_expandable $url) {
+        http_head $url, recurse => 0, sub {
+            my ($data, $headers) = @_;
+            $cb->($headers->{location});
+        };
+        return 1;
+    } else {
+        $cb->();
+        return undef;
+    }
+}
+
 sub _build_guard {
     my $self = shift;
     my $config = pit_get('twitter.com');
@@ -39,8 +69,17 @@ sub _build_guard {
                 my (undef, $json) = @_;
                 return unless defined $json->{text};
                 $logger->log(debug => "$json->{user}->{screen_name}: $json->{text}");
-                my ($url) = $json->{text} =~ m<(http://nico\.ms/sm\d+)>;
-                $self->queue->push($url) if $url;
+                for my $url ($json->{text} =~ /$RE{URI}{HTTP}/go) {
+                    if (url_is_expandable $url) {
+                        $logger->log(info => "found URL $url");
+                        async {
+                            expand_short_url $url, Coro::rouse_cb;
+                            my ($expanded) = Coro::rouse_wait or return;
+                            $logger->log(info => "$url => $expanded");
+                            $self->queue->push($expanded) if $expanded =~ /www\.nicovideo\.jp/; # XXX
+                        };
+                    }
+                }
             };
             $hdl->on_read(sub { $hdl->push_read(json => $r) });
         },
