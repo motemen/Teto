861ad2bb7b5153ee6f8f1913b096528e6550eb3d
motemen <motemen@gmail.com>
2010-11-02 22:15:58 +0900

    セットアップに必要なの

--- teto.pl
+++ teto.pl
@@ -5,6 +5,13 @@ use lib 'lib';
 
 use Teto;
 
+use Config::Pit;
+
+pit_get('nicovideo.jp', require => {
+    username => 'nicovideo email/username',
+    password => 'nicovideo password',
+});
+
 if ($0 eq __FILE__) {
     require Plack::Runner;
     my $teto = Teto->new_with_options;
